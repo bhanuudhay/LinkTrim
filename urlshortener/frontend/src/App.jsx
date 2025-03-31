180831dcb17aff291cad28a36d946d6a03b02f00
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-03-31 17:47:11 +0530

    Create App.jsx

---
+++ urlshortener/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UrlShortener from "./components/UrlShortener";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🔗 URL Shortener</h1>
+      <UrlShortener />
+    </div>
+  );
+}
+
+export default App;
